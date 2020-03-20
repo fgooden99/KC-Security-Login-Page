56c0770820b2e1ab95fb846d089ba509d8b74af1
fgooden99 <fletcherdragon123@gmail.com>
2020-03-20 12:26:52 -0500

    Account controller, error.cshtml, login, forgotpassword, register are the main changes.

---
+++ Login Page/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Login_Page.Startup))]
+namespace Login_Page
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
